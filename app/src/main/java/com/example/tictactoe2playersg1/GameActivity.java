02c811d886291e81ac8d4d970de32e9fbb2aa329
User <israel.kugel10@gmailo.com>
2025-04-06 17:27:34 +0300

    add Firebase

--- app/src/main/java/com/example/tictactoe2playersg1/GameActivity.java
+++ app/src/main/java/com/example/tictactoe2playersg1/GameActivity.java
@@ -1,6 +1,7 @@
 package com.example.tictactoe2playersg1;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,12 +11,24 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class GameActivity extends AppCompatActivity {
 
+    private FbModule fbModule;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        fbModule = new FbModule(this);
+
         //setContentView(R.layout.activity_game);
         BoardGame boardGame = new BoardGame(this);
         setContentView(boardGame);
 
+    }
+
+    public void setPositionToFb(int line, int col) {
+        fbModule.setPositionToFirebase(new Position(line,col));
+    }
+
+    public void setPositionFromFb(Position position) {
+        Toast.makeText(this, "" + position.getCol(), Toast.LENGTH_SHORT).show();
     }
 }
